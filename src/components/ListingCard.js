226fc4d896ac3c94397a3539d4b60b0578ff07a0
kush Muhamad <kushmohammed98@yahoo.com>
2023-09-25 23:50:47 +0300

    monday edit

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React ,{useState} from "react";
 
-function ListingCard() {
+function ListingCard({listing}) {
+
+  const [favorite, setFavorite] = useState(false)
+
+
+  const handleFavorite = () => {
+    setFavorite(!favorite)
+  }
+
+
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image} alt={listing.description} />
       </div>
-      <div className="details">
-        {true ? (
+      <div on onClick={handleFavorite}className="details">
+        {favorite  ?   (
           <button className="emoji-button favorite active">★</button>
         ) : (
           <button className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{listing.description}</strong>
+        <span>  {listing.location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,10 +1,25 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState, useEffect} from "react";
+ import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+
+  const [listing , setListing] = useState([])
+
+
+ useEffect(()=>{
+  fetch("http://localhost:6001/listings")
+  .then(res => res.json())
+  .then(setListing)
+
+ },[])
+
+  
+
+
   return (
     <main>
       <ul className="cards">
+       {listing.map(listing => <ListingCard listing ={listing} key={listing.id}/>)} 
         {/* use the ListingCard component to display listings */}
       </ul>
     </main>
